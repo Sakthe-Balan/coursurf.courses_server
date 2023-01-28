508256da1f8f05e499a77a89ceadf7da47e5e40d
Adithya S K <adithyaskolavi@gmail.com>
2023-01-28 16:39:08 +0530

    added input functionality to scraper - Adithya S K

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 import time 
 
 path = "msedgedriver.exe"
@@ -11,7 +11,14 @@ browser.get(website)
 
 browser.maximize_window()
 
+browser.implicitly_wait(5)
 
+# inputSearch = browser.find_element('//input[@placeholder="Search 100,000 courses…"]')
+inputSearch = browser.find_element(By.XPATH, '//input[@placeholder="Search 100,000 courses…"]')
+inputSearch.send_keys("python")
+inputSearch.submit()
+
+time.sleep(5)
 
 browser.quit()
 
