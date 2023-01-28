4314eb49fe13183b694a58365d4f3c487d43f42e
Adithya S K <adithyaskolavi@gmail.com>
2023-01-29 00:32:24 +0530

    clicked the load more button to reveal 1.5k courses have to optimize it - Adithya S K

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import time 
 
 path = "msedgedriver.exe"
-website = "https://www.classcentral.com/"
-
 browser = webdriver.Edge(path)
 
-browser.get(website)
+website = "https://www.classcentral.com/"
 
-browser.maximize_window()
+provider = "coursera"
 
-browser.implicitly_wait(5)
+# search = input("Enter the course you want to search: ")
+wait = WebDriverWait(browser, 10)
 
-# inputSearch = browser.find_element('//input[@placeholder="Search 100,000 courses…"]')
-inputSearch = browser.find_element(By.XPATH, '//input[@placeholder="Search 100,000 courses…"]')
-inputSearch.send_keys("python")
-inputSearch.submit()
+def topicSearch(search):
+  browser.get(website)
+  browser.maximize_window()
+  # inputSearch = browser.find_element('//input[@placeholder="Search 100,000 courses…"]')
+  inputSearch = browser.find_element(By.XPATH, '//input[@placeholder="Search 100,000 courses…"]')
+  inputSearch.send_keys(search)
+  inputSearch.submit()
 
-time.sleep(5)
+  time.sleep(2)
+  
+  print("Courses loaded")
+  browser.quit()
 
-browser.quit()
 
+def courseProvider(provider):
+  courseProviderUrl = "https://www.classcentral.com/provider/" + provider
+  browser.get(courseProviderUrl)
+  browser.maximize_window()
+  browser.implicitly_wait(5)
+  time.sleep(1)
+  
+  loadMoreButton = browser.find_element(By.XPATH, '//*[@id="page-provider"]/div[1]/div[3]/div[5]/div/button')
+  
+  
+  for i in range(1, 10):
+    while True:
+      try:
+        loadMoreButton.click()
+        time.sleep(0.45+(i/50))
+        # loadMoreButton = browser.find_element(By.XPATH, '//*[@id="page-provider"]/div[1]/div[3]/div[5]/div/button')
+        loadMoreButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page-provider"]/div[1]/div[3]/div[5]/div/button')))
+      except:
+        print("No more courses to load")
+        break
+      
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5+i)
+    loadMoreButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page-provider"]/div[1]/div[3]/div[5]/div/button')))
+    time.sleep(1+i)
+    
+    # time.sleep(2)
+  time.sleep(30)
+  print("Courses loaded")
+  # browser.quit()
+
+courseProvider("coursera")
